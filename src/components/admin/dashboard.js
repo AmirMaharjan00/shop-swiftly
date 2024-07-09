a0e077dc93da5f5c3ece20571b8a707e22e7cef1
Amir Maharjan <mhrznamir.am@gmail.com>
2024-07-09 21:27:02 +0545

    off canvas

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import './assets/css/admin.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faGauge, faPhotoFilm, faFile, faUserTie, faGear, faThumbtack } from '@fortawesome/free-solid-svg-icons'
 
 export default function Admin() {
     return (
@@ -19,29 +19,40 @@ export default function Admin() {
 
 const Sidebar = () => {
     const [ canvasActive, setCanvasActive ] = useState( false )
+    const [ activePage, setActivePage ] = useState( 0 )
     let sidebarItemsArray = [ 
-        { 'label': 'dashboard', 'link': '/swt-admin' },
-        { 'label': 'products', 'link': '/swt-admin/products' },
-        { 'label': 'media', 'link': '/swt-admin/media' },
-        { 'label': 'pages', 'link': '/swt-admin/pages' },
-        { 'label': 'users', 'link': '/swt-admin/users' },
-        { 'label': 'settings', 'link': '/swt-admin/settings' }
+        { 'label': 'dashboard', 'link': '/swt-admin', 'icon': faGauge },
+        { 'label': 'products', 'link': '/swt-admin/products', 'icon': faThumbtack },
+        { 'label': 'media', 'link': '/swt-admin/media', 'icon': faPhotoFilm },
+        { 'label': 'pages', 'link': '/swt-admin/pages', 'icon': faFile },
+        { 'label': 'users', 'link': '/swt-admin/users', 'icon': faUserTie },
+        { 'label': 'settings', 'link': '/swt-admin/settings', 'icon': faGear }
     ]
     return (
         <>
-            <div className='swt-admin-sidebar'>
+            <div className={ 'swt-admin-sidebar' + ( canvasActive ? ' active' : '' ) }>
                 <div className='sidebar-head'>
-                    <h2 className='title'>Shop Swiftly</h2>
+                    { ! canvasActive && <h2 className='title'>{ 'Shop Swiftly' }</h2> }
                     <FontAwesomeIcon
                         icon = { faBars } 
-                        className = { 'off-canvas' + ( canvasActive ? ' active' : '' ) }
+                        className = 'off-canvas'
                         onClick = {() => setCanvasActive( ! canvasActive ) }
                     />
                 </div>
                 <div className='sidebar-body'>
                     <ul className='sidebar-items'>
                         { sidebarItemsArray.map(( element, index ) =>{ 
-                            return ( <Link to={element.link} key={ index }><li className='sidebar-item'>{ element.label.charAt(0).toUpperCase() + element.label.slice(1) }</li></Link> )
+                            return ( 
+                                <Link to={element.link} key={ index } className={ 'sidebar-item-wrapper' + ( activePage == index ? ' active' : '' )} onClick={() => setActivePage( index ) }>
+                                    <li className='sidebar-item'>
+                                        <FontAwesomeIcon
+                                            icon = { element.icon } 
+                                            className = 'off-canvas'
+                                        />
+                                        { ! canvasActive && <span className='menu-label'>{ element.label.charAt(0).toUpperCase() + element.label.slice(1) }</span> }
+                                    </li>
+                                </Link>
+                            )
                         }) }
                     </ul>
                 </div>
