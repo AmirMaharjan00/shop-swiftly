40784f7334729cd4ed9ea91764dad30ca3f42012
Amir Maharjan <mhrznamir.am@gmail.com>
2024-08-01 10:02:17 +0545

    adding add to cart

--- src/components/content/header.js
+++ src/components/content/header.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react'
 import './assets/css/main.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faSun, faMoon, faUserTie } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faSun, faMoon, faUserTie, faCartShopping, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
     const [ getPages, setPages ] = useState([]);
     const [ isSearchActive, setIsSearchActive ] = useState( false )
     const [ isLightMode, setIsLightMode ] = useState( true )
     const [ isLoginPopupActive, setIsLoginPopupActive ] = useState( false )
+    const [ isShoppingCartActive, setIsShoppingCartActive ] = useState( false )
 
     useEffect(() => {
         const FORMDATA = new FormData()
@@ -38,15 +39,72 @@ export default function Header() {
                         }
                     </nav>
                     <div className='site-actions'>
-                        <FontAwesomeIcon icon={ faMagnifyingGlass } className='site-action site-search' onClick={() => setIsSearchActive( ! isSearchActive ) }/>
-                        { isSearchActive && <form onSubmit="" className='header-search-form'>
-                            <input type="search" placeholder="Search..." />
-                        </form> }
-                        <FontAwesomeIcon icon={ isLightMode ? faSun : faMoon } className='site-action site-theme-mode' onClick={() => setIsLightMode( ! isLightMode ) }/>
-                        <FontAwesomeIcon icon={ faUserTie } className='site-action site-user' onClick={() => setIsLoginPopupActive( ! isLoginPopupActive ) }/>
+                        <SearchBox isSearchActive={ isSearchActive } setIsSearchActive={ setIsSearchActive }/>
+                        <ThemeMode isLightMode={ isLightMode } setIsLightMode={ setIsLightMode }/>
+                        <ShoppingCart isShoppingCartActive={ isShoppingCartActive } setIsShoppingCartActive={ setIsShoppingCartActive }/>
+                        <UserLogin isLoginPopupActive={ isLoginPopupActive } setIsLoginPopupActive={ setIsLoginPopupActive }/>
                     </div>
                 </div>
             </div>
         </header>
+    )
+}
+
+const SearchBox = ({ isSearchActive, setIsSearchActive }) => {
+    return(
+        <div className='search-box-wrapper'>
+            <FontAwesomeIcon icon={ faMagnifyingGlass } className='site-action site-search' onClick={() => setIsSearchActive( ! isSearchActive ) }/>
+            { isSearchActive && <form onSubmit="" className='header-search-form'>
+                <input type="search" placeholder="Search..." />
+            </form> }
+        </div>
+    )
+}
+
+const ThemeMode = ({ isLightMode, setIsLightMode }) => {
+    return(
+        <div className='theme-mode-wrapper'>
+            <FontAwesomeIcon icon={ isLightMode ? faSun : faMoon } className='site-action site-theme-mode' onClick={() => setIsLightMode( ! isLightMode ) }/>
+        </div>
+    )
+}
+
+const ShoppingCart = ({ isShoppingCartActive, setIsShoppingCartActive }) => {
+    const cartArray = [ 1, 2, 3, 4, 5 ]
+    return(
+        <div className='shopping-cart-wrapper'>
+            <FontAwesomeIcon icon={ faCartShopping } className='site-action site-user' onClick={() => setIsShoppingCartActive( ! isShoppingCartActive ) }/>
+            { isShoppingCartActive && <div className='cart-popup-wrapper'>
+                {
+                    cartArray.map(( current ) => {
+                        return(
+                            <div className='item'>
+                                <figure className='post-thumb-wrapper no-post-thumb'>
+                                    <img src="" alt=""/>
+                                </figure>
+                                <div className='post-elements'>
+                                    <h2 className='post-title'>{ 'Amir Maharjan' }</h2>
+                                    <span className='post-price'>{ 'Rs. 100' }</span>
+                                    <div className='quantity-wrapper'>
+                                        <button className='quantity-button decrease'><FontAwesomeIcon icon={ faMinus }/></button>
+                                        <span className='quantity-indicator'>{ current }</span>
+                                        <button className='quantity-button increase'><FontAwesomeIcon icon={ faPlus }/></button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                <button className='checkout-button'>{ 'Checkout' }</button>
+            </div> }
+        </div>
+    )
+}
+
+const UserLogin = ({ isLoginPopupActive, setIsLoginPopupActive }) => {
+    return(
+        <div className='user-login-wrapper'>
+            <FontAwesomeIcon icon={ faUserTie } className='site-action site-user' onClick={() => setIsLoginPopupActive( ! isLoginPopupActive ) }/>
+        </div>
     )
 }
